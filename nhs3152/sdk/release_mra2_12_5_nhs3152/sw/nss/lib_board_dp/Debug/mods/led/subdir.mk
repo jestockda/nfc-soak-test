ae44f05d169e12529819d9c60399825776a06455
jestockda <78284798+jestockda@users.noreply.github.com>
2022-12-07 16:31:47 -0500

    projects

--- nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/lib_board_dp/Debug/mods/led/subdir.mk
+++ nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/lib_board_dp/Debug/mods/led/subdir.mk
@@ -4,7 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-//MAC/Home/Documents/GitHub/nfc-soak-test/nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/mods/led/led.c 
+C:/Users/jesmith/Documents/GitHub/nfc-soak-test/nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/mods/led/led.c 
 
 OBJS += \
 ./mods/led/led.o 
@@ -14,10 +14,10 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-mods/led/led.o: //MAC/Home/Documents/GitHub/nfc-soak-test/nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/mods/led/led.c
+mods/led/led.o: C:/Users/jesmith/Documents/GitHub/nfc-soak-test/nhs3152/sdk/release_mra2_12_5_nhs3152/sw/nss/mods/led/led.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -std=c99 -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M0PLUS -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss" -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\inc" -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\mods" -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\inc" -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\mods" -I"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\mods" -include"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\mods\chip_sel.h" -include"\\MAC\Home\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\mods\board_sel.h" -Og -g3 -pedantic -Wall -Wextra -Wconversion -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -std=c99 -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M0PLUS -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss" -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\inc" -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\mods" -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\inc" -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\mods" -I"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\mods" -include"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_chip_nss\mods\chip_sel.h" -include"C:\Users\jesmith\Documents\GitHub\nfc-soak-test\nhs3152\sdk\release_mra2_12_5_nhs3152\sw\nss\lib_board_dp\mods\board_sel.h" -Og -g3 -pedantic -Wall -Wextra -Wconversion -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0plus -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
