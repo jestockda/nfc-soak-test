140fc3b0befe3456b2ef951063ff5d28b0894c0e
jestockda <78284798+jestockda@users.noreply.github.com>
2022-12-13 19:38:46 -0500

    all sorts of updates

--- app_nfc_resistance/src/main.c
+++ app_nfc_resistance/src/main.c
@@ -18,27 +18,60 @@
 #include "ndeft2t/ndeft2t.h"
 
 #define LOCAL "en"
-#define MIME "nhs31xx/example.ndef"
+#define MIME1 "nhs31xx/mime1"
+#define MIME2 "nhs31xx/mime2"
 
 #define MAX_TEXT_PAYLOAD (254 - (NDEFT2T_MSG_OVERHEAD(true, \
         NDEFT2T_TEXT_RECORD_OVERHEAD(true, sizeof(LOCAL) - 1) + \
-        NDEFT2T_MIME_RECORD_OVERHEAD(true, sizeof(MIME) - 1)) / 2))
+        NDEFT2T_MIME_RECORD_OVERHEAD(true, sizeof(MIME1) - 1)) / 2))
 static uint8_t sText[MAX_TEXT_PAYLOAD] = "default text";
 
-#define MAX_MIME_PAYLOAD MAX_TEXT_PAYLOAD
-static uint8_t sBytes[MAX_MIME_PAYLOAD] = {0, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE};
+#define MAX_MIME_PAYLOAD (254 - (NDEFT2T_MSG_OVERHEAD(true, NDEFT2T_MIME_RECORD_OVERHEAD(true, sizeof(MIME1) - 1))))
+
+static uint8_t sBytes1[MAX_MIME_PAYLOAD];
+static uint8_t sBytes2[MAX_MIME_PAYLOAD];
+static uint8_t sBytes3[MAX_MIME_PAYLOAD];
+static uint8_t sBytes4[MAX_MIME_PAYLOAD];
+
+#define SP0_ADDR   0x4001c010
+#define READ_SP0()     (*(volatile uint32_t *)SP0_ADDR)
+#define WRITE_SP0(val) ((*(volatile uint32_t *)SP0_ADDR) = (val))
 
 static volatile bool sMsgAvailable = false;
 static volatile bool sFieldPresent = false;
 
-static volatile float AN4 = 0.0;
+static volatile bool flag = false;
+
+//static volatile float AN4 = 0.0;
 static volatile int current_native;
 static volatile int current_pA;
 static volatile double resistance = -1;
 
-int N = 50;
-int A = 1900;
-int O = 2048;
+int volatile N = 200;
+
+int pre [] = {0x880,0x900,0x97F,0x9FD,0xA78,0xAF1,0xB67,0xBDA,0xC49,0xCB3,
+		       0xD19,0xD79,0xDD4,0xE2A,0xE78,0xEC1,0xF02,0xF3D,0xF70,0xF9B,
+			   0xFBF,0xFDB,0xFEF,0xFFB,0x1000,0xFFB,0xFEF,0xFDB,0xFBF,0xF9B,
+			   0xF70,0xF3D,0xF02,0xEC1,0xE78,0xE2A,0xDD4,0xD79,0xD19,0xCB3,
+			   0xC49,0xBDA,0xB67,0xAF1,0xA78,0x9FD,0x97F,0x900,0x880,0x7FF,
+			   0x77F,0x6FF,0x680,0x602,0x587,0x50E,0x498,0x425,0x3B6,0x34C,
+			   0x2E6,0x286,0x22B,0x1D5,0x187,0x13E,0xFD,0xC2,0x8F,0x64,
+			   0x40,0x24,0x10,0x4,0x0,0x4,0x10,0x24,0x40,0x64,
+			   0x8F,0xC2,0xFD,0x13E,0x187,0x1D5,0x22B,0x286,0x2E6,0x34C,
+			   0x3B6,0x425,0x498,0x50E,0x587,0x602,0x680,0x6FF,0x77F,0x800,
+			   0x880,0x900,0x97F,0x9FD,0xA78,0xAF1,0xB67,0xBDA,0xC49,0xCB3,
+			   0xD19,0xD79,0xDD4,0xE2A,0xE78,0xEC1,0xF02,0xF3D,0xF70,0xF9B,
+			   0xFBF,0xFDB,0xFEF,0xFFB,0x1000,0xFFB,0xFEF,0xFDB,0xFBF,0xF9B,
+			   0xF70,0xF3D,0xF02,0xEC1,0xE78,0xE2A,0xDD4,0xD79,0xD19,0xCB3,
+			   0xC49,0xBDA,0xB67,0xAF1,0xA78,0x9FD,0x97F,0x900,0x880,0x800,
+			   0x77F,0x6FF,0x680,0x602,0x587,0x50E,0x498,0x425,0x3B6,0x34C,
+			   0x2E6,0x286,0x22B,0x1D5,0x187,0x13E,0xFD,0xC2,0x8F,0x64,
+			   0x40,0x24,0x10,0x4,0x0,0x4,0x10,0x24,0x40,0x64,
+			   0x8F,0xC2,0xFD,0x13E,0x187,0x1D5,0x22B,0x286,0x2E6,0x34C,
+			   0x3B6,0x425,0x498,0x50E,0x587,0x602,0x680,0x6FF,0x77F,0x800};
+
+static volatile uint16_t AN2 [200];
+static volatile uint16_t AN4 [200];
 
 static void set_dac(void);
 static void get_i2d(void);
@@ -48,13 +81,16 @@ static void GenerateNdef_TextMime(void);
 
 void App_FieldStatusCb(bool status)
 {
-    if (status) {
+    /*
+	if (status) {
         LED_On(LED_RED);
     }
     else {
         LED_Off(LED_RED);
     }
+    */
     sFieldPresent = status; /* Handled in main loop */
+
 }
 
 void App_MsgAvailableCb(void)
@@ -62,32 +98,58 @@ void App_MsgAvailableCb(void)
     sMsgAvailable = true; /* Handled in main loop */
 }
 
-static void GenerateNdef_Text(void)
+static void GenerateNdef_Mime_1(void)
 {
     uint8_t instance[NDEFT2T_INSTANCE_SIZE];
-    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE ];
-    NDEFT2T_CREATE_RECORD_INFO_T textRecordInfo = {.pString = (uint8_t *)"en" /* language code */,
-                                                   .shortRecord = true,
+    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE];
+    NDEFT2T_CREATE_RECORD_INFO_T mimeRecordInfo = {.pString = (uint8_t *)MIME1,
+                                                   .shortRecord = true, // <<<<< THIS
                                                    .uriCode = 0 /* don't care */};
 
     NDEFT2T_CreateMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE, true);
-    if (NDEFT2T_CreateTextRecord(instance, &textRecordInfo)) {
-        if (NDEFT2T_WriteRecordPayload(instance, sText, sizeof(sText) - 1 /* exclude NUL char */)) {
+    if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
+        if (NDEFT2T_WriteRecordPayload(instance, sBytes1, sizeof(sBytes1))) {
             NDEFT2T_CommitRecord(instance);
         }
     }
+    if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
+        if (NDEFT2T_WriteRecordPayload(instance, sBytes2, sizeof(sBytes2))) {
+            NDEFT2T_CommitRecord(instance);
+        }
+    }
+    NDEFT2T_CommitMessage(instance); /* Copies the generated message to NFC shared memory. */
+}
 
+static void GenerateNdef_Mime_2(void)
+{
+    uint8_t instance[NDEFT2T_INSTANCE_SIZE];
+    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE];
+    NDEFT2T_CREATE_RECORD_INFO_T mimeRecordInfo = {.pString = (uint8_t *)MIME2,
+                                                   .shortRecord = true, // <<<<< THIS
+                                                   .uriCode = 0 /* don't care */};
+
+    NDEFT2T_CreateMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE, true);
+    if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
+        if (NDEFT2T_WriteRecordPayload(instance, sBytes3, sizeof(sBytes3))) {
+            NDEFT2T_CommitRecord(instance);
+        }
+    }
+    if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
+        if (NDEFT2T_WriteRecordPayload(instance, sBytes4, sizeof(sBytes4))) {
+            NDEFT2T_CommitRecord(instance);
+        }
+    }
     NDEFT2T_CommitMessage(instance); /* Copies the generated message to NFC shared memory. */
 }
 
 static void GenerateNdef_TextMime(void)
 {
     uint8_t instance[NDEFT2T_INSTANCE_SIZE];
-    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE ];
+    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE];
     NDEFT2T_CREATE_RECORD_INFO_T textRecordInfo = {.pString = (uint8_t *)"en" /* language code */,
                                                    .shortRecord = true,
                                                    .uriCode = 0 /* don't care */};
-    NDEFT2T_CREATE_RECORD_INFO_T mimeRecordInfo = {.pString = (uint8_t *)MIME /* mime type */,
+    NDEFT2T_CREATE_RECORD_INFO_T mimeRecordInfo = {.pString = (uint8_t *)MIME1 /* mime type */,
                                                    .shortRecord = true,
                                                    .uriCode = 0 /* don't care */};
     NDEFT2T_CreateMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE, true);
@@ -97,12 +159,14 @@ static void GenerateNdef_TextMime(void)
         }
     }
     if (NDEFT2T_CreateMimeRecord(instance, &mimeRecordInfo)) {
-        if (NDEFT2T_WriteRecordPayload(instance, sBytes, sizeof(sBytes))) {
+        if (NDEFT2T_WriteRecordPayload(instance, sBytes1, sizeof(sBytes1))) {
             NDEFT2T_CommitRecord(instance);
         }
     }
     NDEFT2T_CommitMessage(instance); /* Copies the generated message to NFC shared memory. */
 }
+
+/* ============================================== */
 
 void set_dac(void){
 	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_1, IOCON_FUNC_1);
@@ -110,21 +174,6 @@ void set_dac(void){
 	Chip_ADCDAC_SetMuxDAC(NSS_ADCDAC0, ADCDAC_IO_ANA0_1);
 	Chip_ADCDAC_SetModeDAC(NSS_ADCDAC0, ADCDAC_CONTINUOUS); // hold
 	Chip_ADCDAC_WriteOutputDAC(NSS_ADCDAC0, 4095); // max
-}
-
-void init_dac(void){
-	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_0, IOCON_FUNC_1);
-	Chip_ADCDAC_Init(NSS_ADCDAC0);
-	Chip_ADCDAC_SetMuxDAC(NSS_ADCDAC0, ADCDAC_IO_ANA0_0);
-	Chip_ADCDAC_SetModeDAC(NSS_ADCDAC0, ADCDAC_CONTINUOUS); // hold
-}
-
-void sine_dac(void){
-	for (int i = 0; i < N; i++){
-		int out = A*sin(i*6.28/N) + O;
-		Chip_ADCDAC_WriteOutputDAC(NSS_ADCDAC0, out);
-		//Chip_Clock_System_BusyWait_us(1);
-	}
 }
 
 void get_i2d(void){
@@ -162,8 +211,68 @@ void get_adc(void){
 	Chip_ADCDAC_SetModeADC(NSS_ADCDAC0, ADCDAC_SINGLE_SHOT);
 	Chip_ADCDAC_StartADC(NSS_ADCDAC0);
 	while (!(Chip_ADCDAC_ReadStatus(NSS_ADCDAC0) & ADCDAC_STATUS_ADC_DONE)) {;} // wait
-	AN4 = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
-	AN4 = (AN4/4095.0)*1.6;
+	//AN4 = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
+    //AN4 = (AN4/4095.0)*1.6;
+}
+
+/* ============================================== */
+
+void init_adcdac(void){
+	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_0, IOCON_FUNC_1);
+	Chip_ADCDAC_Init(NSS_ADCDAC0);
+	Chip_ADCDAC_SetMuxDAC(NSS_ADCDAC0, ADCDAC_IO_ANA0_0);
+	Chip_ADCDAC_SetInputRangeADC(NSS_ADCDAC0, ADCDAC_INPUTRANGE_WIDE);
+	Chip_ADCDAC_SetModeDAC(NSS_ADCDAC0, ADCDAC_CONTINUOUS); // hold
+
+	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_2, IOCON_FUNC_1);
+	Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_ANA0_4, IOCON_FUNC_1);
+	Chip_ADCDAC_SetMuxADC(NSS_ADCDAC0, ADCDAC_IO_ANA0_2);
+	Chip_ADCDAC_SetInputRangeADC(NSS_ADCDAC0, ADCDAC_INPUTRANGE_WIDE);
+	Chip_ADCDAC_SetModeADC(NSS_ADCDAC0, ADCDAC_CONTINUOUS);
+	Chip_ADCDAC_StartADC(NSS_ADCDAC0);
+
+    Chip_Flash_SetHighPowerMode(true);
+    bool check_pm = Chip_Flash_GetHighPowerMode();
+
+    //Chip_Flash_SetNumWaitStates(1);
+    //int check_ws = Chip_Flash_GetNumWaitStates();
+
+    if (check_pm){
+    	Chip_Clock_System_SetClockDiv(1);
+    }
+    else{
+    	Chip_Clock_System_SetClockDiv(2);
+	}
+
+    WRITE_SP0(0x2);
+}
+
+void sine_dac_fast(void){
+
+
+	//Chip_ADCDAC_StopADC(NSS_ADCDAC0);
+
+	//Chip_ADCDAC_StartADC(NSS_ADCDAC0);
+
+	//while (!(Chip_ADCDAC_ReadStatus(NSS_ADCDAC0) & ADCDAC_STATUS_ADC_DONE)) {;} // wait
+
+	for (int i = 0; i < N; i++){
+		Chip_ADCDAC_WriteOutputDAC(NSS_ADCDAC0, pre[i]);
+		AN2[i] = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
+	}
+
+	//Chip_ADCDAC_StopADC(NSS_ADCDAC0);
+	Chip_ADCDAC_SetMuxADC(NSS_ADCDAC0, ADCDAC_IO_ANA0_4);
+	//Chip_ADCDAC_StartADC(NSS_ADCDAC0);
+
+	//while (!(Chip_ADCDAC_ReadStatus(NSS_ADCDAC0) & ADCDAC_STATUS_ADC_DONE)) {;} // wait
+
+	for (int i = 0; i < N; i++){
+		Chip_ADCDAC_WriteOutputDAC(NSS_ADCDAC0, pre[i]);
+		AN4[i] = Chip_ADCDAC_GetValueADC(NSS_ADCDAC0);
+	}
+
+	Chip_ADCDAC_SetMuxADC(NSS_ADCDAC0, ADCDAC_IO_ANA0_2);
 }
 
 void pwm_dev(void)
@@ -209,36 +318,91 @@ void pwm_dev(void)
     Chip_TIMER_Enable(NSS_TIMER16_0);
 }
 
-/*
-void pwm_sine(void){
-	for (int i = 0; i < N; i++){
-		int out = A*sin(i*6.28/N) + O;
-	    Chip_TIMER_SetMatch(NSS_TIMER16_0, 0, 63);
-		//Chip_Clock_System_BusyWait_us(1);
-	}
+static void ParseNdef(void)
+{
+    uint8_t instance[NDEFT2T_INSTANCE_SIZE];
+    uint8_t buffer[NFC_SHARED_MEM_BYTE_SIZE];
+    NDEFT2T_PARSE_RECORD_INFO_T recordInfo;
+    int len = 0;
+    uint8_t *pData = NULL;
+
+    if (NDEFT2T_GetMessage(instance, buffer, NFC_SHARED_MEM_BYTE_SIZE)) {
+        while (NDEFT2T_GetNextRecord(instance, &recordInfo) != false) {
+            pData = (uint8_t *)NDEFT2T_GetRecordPayload(instance, &len);
+            switch (recordInfo.type) {
+                case NDEFT2T_RECORD_TYPE_TEXT:
+                    if ((size_t)len <= MAX_TEXT_PAYLOAD) {
+                        memcpy(sText, pData, (size_t)len);
+                        memset(sText + len, 0, MAX_TEXT_PAYLOAD - (size_t)len);
+                    }
+                    break;
+                default:
+                    /* ignore */
+                    break;
+            }
+        }
+    }
 }
-*/
 
 int main(void)
 {
     Board_Init();
-    //NDEFT2T_Init();
-    //Chip_Clock_System_BusyWait_ms(50);
-    //pwm_dev();
-    init_dac();
+    NDEFT2T_Init();
+    init_adcdac();
 
     for (;;) {
 
-    	sine_dac();
+    	sine_dac_fast();
 
-    	/*
+    	int q = 0;
+    	for (int i = 0; i<N/2; i++){
+    		sBytes1[q]   = (uint8_t)(AN2[i] >>  0);
+    		sBytes1[q+1] = (uint8_t)(AN2[i] >>  8);
+
+		    sBytes2[q]   = (uint8_t)(AN2[i+100] >>  0);
+		    sBytes2[q+1] = (uint8_t)(AN2[i+100] >>  8);
+
+		    sBytes3[q]   = (uint8_t)(AN4[i] >>  0);
+		    sBytes3[q+1] = (uint8_t)(AN4[i] >>  8);
+
+		    sBytes4[q]   = (uint8_t)(AN4[i+100] >>  0);
+		    sBytes4[q+1] = (uint8_t)(AN4[i+100] >>  8);
+
+		    q+=2;
+    	}
+
+    	if (!flag){
+    		GenerateNdef_Mime_1();
+    		flag = true;
+    	}
+
+    	while (!sMsgAvailable) {;}
+		if (sMsgAvailable){
+         	sMsgAvailable = false;
+    		ParseNdef();
+		}
+		if (sText[0]=='a'){
+			sText[0]='x';
+			GenerateNdef_Mime_2();
+		}
+    	while (!sMsgAvailable) {;}
+		if (sMsgAvailable){
+         	sMsgAvailable = false;
+    		ParseNdef();
+		}
+		if (sText[0]=='b'){
+			sText[0]='x';
+			flag = false;
+		}
+
+        /*
     	set_dac();
         get_i2d();
         get_adc();
         resistance = (1.6-AN4)/(current_pA*1e-12);
         sprintf((char *)sText, "%6.4f,%8.d,%e", AN4, current_pA, resistance);
         GenerateNdef_TextMime();
-        */
+		*/
     }
     return 0;
 }
--- nfc_reader.py
+++ nfc_reader.py
@@ -1,22 +1,33 @@
 # Interrogate soak testing MCU over NFC
-# Last revised 7 December 2022
+# Last revised 12 December 2022
 # by J. Evan Smith
 
 # ------
 # TODO:
 # (1) list index for tag.ndef.records[0] out of range on first run
-# (2) add error handling for reader disconnect
-# (3) add error handling for no reader on start
+# (2) Reduce to text rather than text+mime —> see ndeft2t_mod_example_1.c 
+# (3) add error handling for reader disconnect
+# (4) add error handling for no reader on start
 
 import nfc
+import plotext as pltx
+import numpy as np
+
 import sys
 import datetime
 import argparse
+
+from scipy.optimize import curve_fit
+
+import ndef
+from ndef import TextRecord
 
 import time
 from time import sleep
 import logging
 from logging import critical, error, info, warning, debug
+
+flagx = True
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Arguments get parsed via --commands')
@@ -26,6 +37,9 @@ def parse_arguments():
 
     parser.add_argument('-f', metavar='file_name', type=str, default='data_output',
         help='File name: use .csv file format')
+
+    parser.add_argument('-t', metavar='period', type=int, default=5,
+        help='Period between subsequent measurements')    
 
     args = parser.parse_args()
     verbose = {0: logging.CRITICAL, 1: logging.ERROR, 2: logging.WARNING, 3: logging.INFO, 4: logging.DEBUG}
@@ -37,6 +51,9 @@ def create_file(file_name):
     f = open(file_name,'w')
     f.write('ADC,I2D,Resistance\n')
     f.close()
+
+def waveform(x,a,b,c):
+    return a*np.sin(b*x)+c
 
 def connect(file_name):
     rdwr_options = {
@@ -50,28 +67,126 @@ def connect(file_name):
     with nfc.ContactlessFrontend('usb:054c') as clf:
 
         tag = clf.connect(rdwr=rdwr_options)
-
         print('connected to tag with type and ID:')
         print(tag)
-        
         print('attempting to retrieve NDEF text ...')
 
         while tag.is_present:
     
-            sleep(2)
+            pltx.clear_figure()
+            count_down(args)
         
             if not tag.ndef:
                 print("No tag or NDEF records found!")
                 continue
 
-            if tag.ndef.has_changed and tag.is_present:
-                f = open(file_name,'a')
-                now = datetime.datetime.now()
-                print (now.strftime("%Y-%m-%d %H:%M:%S"))
-                print(tag.ndef.records[0].text)
-                f.write('{} \n'.format(tag.ndef.records[0].text))
-                f.close()   
-        
+            if tag.is_present: #and tag.ndef.has_changed
+                
+                #try:
+                    f = open(file_name,'a')
+                    now = datetime.datetime.now()
+                    print ("data from " + now.strftime("%Y-%m-%d %H:%M:%S"))
+
+                    for records in tag.ndef.records:
+                        print(records)
+                
+                    a = np.array(tag.ndef.records[0].data) 
+                    b = np.array(tag.ndef.records[1].data)
+                    y1 = np.zeros(200)
+
+                    print('batch 1')
+                    print('sending acknolwedge ...')
+
+                    tag.ndef.records = [TextRecord("a")]
+                
+                    sleep(1)
+                    assert tag.ndef.has_changed is True
+
+                    for records in tag.ndef.records:
+                        print(records)
+
+                    c = np.array(tag.ndef.records[0].data) 
+                    d = np.array(tag.ndef.records[1].data)
+                    y2 = np.zeros(200)
+
+                    print('batch 2')
+                    print('sending acknolwedge ...')
+
+                    tag.ndef.records = [TextRecord("b")]
+
+                    sleep(1)
+                    assert tag.ndef.has_changed is True
+
+                    for k in np.arange(0,100,1):
+                        y1[k] = a[2*k]+256*a[2*k+1]
+                        y1[k+100] = b[2*k]+256*b[2*k+1]
+                        y2[k] = c[2*k]+256*c[2*k+1]
+                        y2[k+100] = d[2*k]+256*d[2*k+1]
+
+                    x = np.linspace(0,len(y1)-1,len(y1))
+                    param1, param_cov1 = curve_fit(waveform,x,y1,[1500, 0.05, 2000])
+                    param2, param_cov2 = curve_fit(waveform,x,y2,[1500, 0.05, 2000])
+
+                    #f.write('{} \n'.format(tag.ndef.records[0].text))
+                    f.close()   
+
+                    pltx.scatter(y1,color='white',marker='braille')
+                    pltx.title('response 1')
+                    pltx_style()
+                    pltx.show()
+                    pltx.clear_figure()
+
+                    pltx.scatter(y2,color='white',marker='braille')
+                    pltx.title('response 2')
+                    pltx_style()
+                    pltx.show()
+                    pltx.clear_figure()
+
+                    pltx.scatter(y1,color='red+',marker='braille')
+                    pltx.scatter(y2,color='blue+',marker='braille')
+                    pltx.title('comparison')
+                    pltx_style()
+                    pltx.show()
+                    pltx.clear_figure()
+
+                    z1 = param1[0]*np.sin(param1[1]*x)+param1[2]
+                    z2 = param2[0]*np.sin(param2[1]*x)+param2[2]
+
+                    pltx.scatter(z1,color='red+',marker='braille')
+                    pltx.scatter(z2,color='blue+',marker='braille')
+                    pltx.title('comparison')
+                    pltx_style()
+                    pltx.show()
+                    pltx.clear_figure()
+
+                    print('parameters from driving wave:')
+                    print(param1)
+
+                    print('parameters from response wave:')
+                    print(param2)
+
+                    vch1 = param1[0]/(2**12)*1.6
+                    vch2 = param2[0]/(2**12)*1.6
+
+                    I = (vch1-vch2)/97.8e3
+                    Z = vch2/I
+
+                    print('impedance estimated to be')
+                    print(Z)
+
+                    #print(param)
+                    #z = param[0]*np.sin(param[1]*x)+param[2]
+                    #pltx.plot(x,z,color='red',marker='braille')
+                    #pltx.title('curve fit results')
+                    #pltx_style()
+                    #pltx.show()
+                    #pltx.clear_figure()
+                
+                #except:
+                #    print('something went wrong! trying again ...')
+                #    continue
+                
+
         print('tag removed (!)')
 
 def on_startup(targets):
@@ -85,6 +200,30 @@ def on_discover(tag):
 def on_release(tag):
     print('tag moved out of range (!)')
     return tag
+
+def pltx_style():
+    pltx.canvas_color('black')
+    pltx.axes_color('black')
+    pltx.ticks_color('white')
+    pltx.xlabel('samples')
+    th = pltx.terminal_height()
+    tw = pltx.terminal_width()
+    pltx.plot_size(0.6*tw, 0.4*th)
+    pltx.ylim(0,4096)
+
+def test_plot():
+    x = np.arange(0,12.56,0.1)
+    y = np.sin(x)
+
+    pltx.plot(x,y,label='sin',color='red',marker='braille')
+    pltx_style()
+    pltx.show()
+
+def count_down(args):
+    N = args.t
+    for i in range(N):
+        sleep(1)
+        print(f"next measurement in {N-i:d} seconds ...", end="\r")    
 
 def main():
     file_name = args.f
